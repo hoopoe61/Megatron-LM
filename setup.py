cba825e6fe637c95ce184a74fda449249fef4bf7
gutao <gutao@kanzhun.com>
2024-04-24 11:43:56 +0800

    add packages

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setuptools.setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
     ],
-    packages=setuptools.find_namespace_packages(include=["megatron.core", "megatron.core.*"]),
+    packages=setuptools.find_namespace_packages(include=["megatron.core", "megatron.core.*", "megatron.inference", "megatron.inference.*", "megatron.legacy", "megatron.legacy.*", "megatron.training", "megatron.training.*"]),
     ext_modules=[
         Extension(
             "megatron.core.datasets.helpers",
