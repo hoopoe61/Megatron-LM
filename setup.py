4e4c513b663eeef541ecd34da70cbdcae84a9c35
gutao <gutao@kanzhun.com>
2025-02-07 16:13:50 +0800

    add all requirements for install megatron

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setuptools.setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
     ],
-    packages=setuptools.find_namespace_packages(include=["megatron.core", "megatron.core.*"]),
+    packages=setuptools.find_namespace_packages(include=["megatron", "megatron.*"]),
     ext_modules=[
         Extension(
             "megatron.core.datasets.helpers_cpp",
@@ -120,8 +120,11 @@ setuptools.setup(
             optional=True,
         )
     ],
+    python_requires=">=3.6",
+    install_requires=install_requires,
     # Add in any packaged data.
     include_package_data=True,
+    zip_safe=False,
     # PyPI package information.
     keywords=__keywords__,
     install_requires=install_requires,
